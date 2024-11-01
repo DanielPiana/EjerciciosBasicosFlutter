7c81ed3049d1f8bd18d63918c97bfdf401e17702
Pepitoelgringo <kxzdanidp@gmail.com>
2024-11-01 20:22:43 +0100

    Ejercicio 11

--- lib/ejercicio11.dart
+++ lib/ejercicio11.dart
@@ -24,11 +24,97 @@ class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => HomePageState();
 }
-
+//CREAMOS LA CLAVE DEL FORMULARIO Y LOS CONTROLADORES PARA CADA CAMPO
 class HomePageState extends State<HomePage> {
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+//METODO PARA LIMPIAR RECURSOS Y AHORRAR MEMORIA
+  void clearResources() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+//METODO PARA VALIDAR UN FORMULARIO
+  void submitForm() {
+    if (formKey.currentState!.validate()) {
+      String nombre = nameController.text;
+      String email = emailController.text;
+      String password = passwordController.text;
+    }
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Formulario enviado correctamente"))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Formulario",
+        style: TextStyle(fontSize: 40,
+            fontWeight:FontWeight.bold)
+      ),
+        centerTitle: true
+      ),
+      body: Padding(
+          padding:
+          const EdgeInsets.all(5),
+        child:
+        Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: "Nombre: "),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Ingresa tu nombre";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: "Correo: "),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Escribe tu correo";
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return "Ingrese un correo válido";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: "Contraseña"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Escribe tu contraseña";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    submitForm();
+                  },
+                  child:
+                  const Text("Enviar"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
 }
+
+
